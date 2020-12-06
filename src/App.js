fdbe9a3b7885f9738b328c8cf9cfadf499443392
sxidsvit <s.p.antonyuk@gmail.com>
2020-12-06 04:40:13 +0200

    db.json

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ const App = () => {
   const [deviceTree, setDeviceTree] = useState({})
   const [deviceTreeTypeMap, setDeviceTreeTypeMap] = useState({})
 
+
+
   useEffect(() => {
-    console.log('useEffect ...')
     async function fetchData(url) {
       try {
         const response = await fetch(url)
         const result = await response.json()
         const data = await result.api
-        console.log('data: ', data);
         const { deviceTree, deviceTreeTypeMap } = await data
 
         localStorage.setItem('deviceTree', JSON.stringify(deviceTree))
@@ -24,23 +24,23 @@ const App = () => {
         console.log(err)
       }
     }
-
-    fetchData('/db.json')
+    fetchData('db.json')
   }, [])
 
-  console.log('deviceTree: ', deviceTree);
   const modNames = []
   for (let key in deviceTree) {
-    console.log('deviceTree[key][0].modName: ', deviceTree[key][0].modName);
     modNames.push(deviceTree[key][0].modName)
   }
-  console.log('modNames: ', modNames);
 
-  const list = 'Sergo'
-  // const list = modNames.map(item => (`<div>${item}</div>`)).json(' ')
+  let list = modNames.map(item => {
+    return `<div>${item}</div>`
+  }).join('')
+  console.log(typeof list);
 
   return (
-    { list }
+    <>
+      {list}
+    </>
   )
 }
 
